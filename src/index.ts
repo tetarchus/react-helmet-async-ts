039aa8dc80ba99b8c9a2d97a1a10a526dedf4298
tetarchus <8436118+tetarchus@users.noreply.github.com>
2022-06-06 13:22:30 +0100

    Renames `HelmetContextProvider` to allow full drop-in replacement

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { Helmet } from './Helmet';
 export { HelmetData } from './HelmetData';
-export { HelmetContextProvider } from './HelmetContext';
+export { HelmetContextProvider as HelmetProvider } from './HelmetContext';
 
 export type { HelmetContext, HelmetProps, HelmetProviderProps } from './types';
